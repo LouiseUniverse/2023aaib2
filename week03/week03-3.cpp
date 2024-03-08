710fc60114bbbc257057156834e65871e1f4040a
LouiseUniverse <universe9462@gmail.com>
2024-03-08 10:05:42 +0800

    week03

---
+++ week03/week03-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    while(a>0){
+        printf("a¬O%d\n",a);
+        a-=1;
+    }
+    for (int b=5; b>0; b=-1){
+        printf("b¬O%d\n",b);
+    }
+}
